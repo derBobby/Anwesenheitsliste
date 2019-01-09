1f61bb09f48386ae5a5f51a93d21b4b0a513a8d5
derBobby <dev@planlos.eu>
2019-01-09 01:49:27 +0100

    Correct handler activated

--- src/main/java/eu/planlos/anwesenheitsliste/security/SecurityConfiguration.java
+++ src/main/java/eu/planlos/anwesenheitsliste/security/SecurityConfiguration.java
@@ -1,13 +1,14 @@
 package eu.planlos.anwesenheitsliste.security;
 
-import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_AREA_ACTUATOR;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_AREA_ADMIN;
+import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_AREA_ACTUATOR;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_AREA_USER;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_LOGIN_FORM;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_LOGIN_FORM_ERROR;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_LOGIN;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_HOME;
 import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_LOGOUT;
+import static eu.planlos.anwesenheitsliste.viewhelper.ApplicationPaths.URL_403;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -26,8 +27,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private LoginAuthenticationSuccessHandler successHandler;
 	
-	@Autowired
-	private LoginAccessDeniedHandler deniedHandler;
+	//@Autowired
+	//private LoginAccessDeniedHandler deniedHandler;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -38,54 +39,55 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/")
 					.permitAll()
 					
-			// Ressources like CSS and JS
+				// Ressources like CSS and JS
 				.antMatchers("/webjars/**", "/sbadmin/**", "/css/**") //TODO
 					.permitAll()
 
-			// User area
+				// User area
 				.antMatchers(URL_AREA_USER + "/**")
 					 .hasAnyAuthority("ROLE_ADMIN", "ROLE_USER") //or authenticated()
 
-			// Admin area
+				// Admin area
 				.antMatchers(URL_AREA_ADMIN + "/**")
 					 .hasAnyAuthority("ROLE_ADMIN") //or authenticated()
-
-			// Technical like actuator
+					 
+				// Technical like actuator
 				.antMatchers(URL_AREA_ACTUATOR + "/**")
 					 .hasAnyAuthority("ROLE_ADMIN") //or authenticated()
 						 
-			// Login procedure
+				// Login procedure
 				.and().formLogin()
 				
-				//Overrides the default created login form site
+					// Overrides the default created login form site
 					.loginPage(URL_LOGIN_FORM)
 					
-				//Names URL on which Spring should listen itself
+					// Names URL on which Spring should listen itself
 					.loginProcessingUrl(URL_LOGIN)
 					
-				//Controller
+					// Controller
 					.successHandler(successHandler)
 					
-				//NOT USED - would redirect to given page, but is handled by 
+					// NOT USED - would redirect to given page, but is handled by 
 					//.defaultSuccessUrl(defaultSuccessUrl, alwaysUse)
 					
-				//Which site to load after login error
+					// Which site to load after login error
 					.failureUrl(URL_LOGIN_FORM_ERROR)
 										
-			// Logout procedure
+				// Logout procedure
 				.and().logout()
 					.logoutUrl(URL_LOGOUT)
 					.logoutSuccessUrl(URL_HOME)
 					.invalidateHttpSession(true)
 					.clearAuthentication(true)
 					
-			// Exception handling
+				// Exception handling
 				.and().exceptionHandling()
 				
-					//Use either own handler or
-					.accessDeniedHandler(deniedHandler); //TODO CONTINUE HERE
-					//... use this default handler
-					//.accessDeniedPage(URL_403);
+					// Use either own handler or
+					//.accessDeniedHandler(deniedHandler); //TODO CONTINUE HERE
+					// ... use this default handler
+					.accessDeniedPage(URL_403)
+					;
 	}
 	
 	@Override

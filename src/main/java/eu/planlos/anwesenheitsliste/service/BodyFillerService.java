89175a80c0226a5c0a16240185016763c5dd8805
derBobby <dev@planlos.eu>
2019-01-14 00:34:17 +0100

    Small security improvement, footer security output only in DEV profile

--- src/main/java/eu/planlos/anwesenheitsliste/service/BodyFillerService.java
+++ src/main/java/eu/planlos/anwesenheitsliste/service/BodyFillerService.java
@@ -1,24 +1,40 @@
 package eu.planlos.anwesenheitsliste.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.context.request.RequestContextHolder;
 
 @Service
-public class BodyFillerService {
+public class BodyFillerService implements EnvironmentAware {
+
+	@Autowired
+	private Environment environment;
 
 	@Autowired
 	private MenuFillerService mc;
 	
 	public void fill(Model model, String module, String title) {
-				
-		model.addAttribute("auth", SecurityContextHolder.getContext().getAuthentication()); 
-		model.addAttribute("session", RequestContextHolder.currentRequestAttributes().getSessionId());
+		
+        for (final String profileName : environment.getActiveProfiles()) {
+            if(profileName.equals("DEV")) {
+        		model.addAttribute("auth", SecurityContextHolder.getContext().getAuthentication()); 
+        		model.addAttribute("mysession", RequestContextHolder.currentRequestAttributes().getSessionId());
+        		break;
+            }
+        }
+
 		model.addAttribute("module", module);
 		model.addAttribute("title", title);		
 		
 		mc.fill(model);
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
 	}
 }

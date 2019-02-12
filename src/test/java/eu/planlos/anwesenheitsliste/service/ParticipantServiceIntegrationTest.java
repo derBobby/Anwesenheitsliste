6de089a83df3adc8859db77ed6b83dbacc9915d9
derBobby <dev@planlos.eu>
2019-02-13 00:07:11 +0100

    Take sum "tests" :D

--- src/test/java/eu/planlos/anwesenheitsliste/service/ParticipantServiceIntegrationTest.java
+++ src/test/java/eu/planlos/anwesenheitsliste/service/ParticipantServiceIntegrationTest.java
@@ -3,34 +3,28 @@ package eu.planlos.anwesenheitsliste.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import eu.planlos.anwesenheitsliste.model.TeamRepository;
-import eu.planlos.anwesenheitsliste.model.User;
-import eu.planlos.anwesenheitsliste.model.UserRepository;
+import eu.planlos.anwesenheitsliste.model.ParticipantRepository;
 
 @RunWith(SpringRunner.class)
-public class UserServiceIntegrationTest {
+public class ParticipantServiceIntegrationTest {
  
     @TestConfiguration
-    static class UserServiceIntegrationTestContextConfiguration {
+    static class ParticipantServiceIntegrationTestContextConfiguration {
 
 	    @Bean
-        public UserService userService() {
-            return new UserService();
+        public ParticipantService participantService() {
+            return new ParticipantService();
         }
     }
  
     @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
-    private TeamRepository teamRepository;
+    private ParticipantRepository participantRepo;
     
     @MockBean
     private SecurityService securityService;
@@ -39,10 +33,10 @@ public class UserServiceIntegrationTest {
 
     @Before
     public void setUp() {
-    	User flastname = new User("Firstname", "Lastname", "flastname", "flastname@example.com", bCryptPasswordEncoder.encode("securepw"), true, false);       
-     
-        Mockito.when(userRepository.findByLoginName(flastname.getLoginName()))
-          .thenReturn(flastname);
+//    	User flastname = new User("Firstname", "Lastname", "flastname", "flastname@example.com", bCryptPasswordEncoder.encode("securepw"), true, false);       
+//     
+//        Mockito.when(userRepository.findByLoginName(flastname.getLoginName()))
+//          .thenReturn(flastname);
     }
     
     @Test

0e4bfde84931d5a98ae3dde348c42e65b6df537e
derBobby <dev@planlos.eu>
2019-05-05 23:15:58 +0200

    New exception class

---
+++ src/main/java/eu/planlos/anwesenheitsliste/model/exception/NotAuthorizedForTeamException.java
@@ -0,0 +1,10 @@
+package eu.planlos.anwesenheitsliste.model.exception;
+
+public class NotAuthorizedForTeamException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 31626335485606637L;
+
+}
